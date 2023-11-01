118cc547103404e290cfdb437390ad68643ea9ec
kkalyankumar9 <kummarikalyan010@gmail.com>
2023-11-01 14:45:37 +0530

    translatorFE

--- Sprint-1/Day3/backend/index.js
+++ Sprint-1/Day3/backend/index.js
@@ -1,10 +1,12 @@
 const express=require("express");
 const { contentRouter } = require("./Route/contentConvertor");
+const cors=require("cors")
 require('dotenv').config()
 
 const app=express();
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cors())
 app.use("/",contentRouter)
 
 
--- Sprint-1/Day3/frontend/texttranslator/src/App.js
+++ Sprint-1/Day3/frontend/texttranslator/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Translate from './Component/translate';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box minHeight="100vh" bgColor={"#e6f7ff"}>
+     <Translate/>
+     </Box>
     </div>
   );
 }
