ad74c7b19ecbfc2447fdfd9b47ae1ef4f07d91ed
kkalyankumar9 <kummarikalyan010@gmail.com>
2023-11-01 12:28:30 +0530

    translateserver

--- Sprint-1/Day3/backend/Route/contentConvertor.js
+++ Sprint-1/Day3/backend/Route/contentConvertor.js
@@ -11,10 +11,10 @@ contentRouter.post("/contentgen",async(req,res)=>{
             "messages": [
                 {
                     "role": "user", 
-                    "content": `Summarize the provided ${text} `
+                    "content": `Generate content on the  ${text} `
                 }
             ],
-            "temperature": 0.7
+           "max_tokens":300
     
         }
      const response= await axios.post("https://api.openai.com/v1/chat/completions",requestbody,
@@ -26,6 +26,69 @@ contentRouter.post("/contentgen",async(req,res)=>{
         const contentGeneration=response.data.choices[0].message.content
         res.json({contentGeneration})
         console.log(contentGeneration)
+
+        
+    } catch (error) {
+        res.status(400).send({"error":error})
+    }
+})
+
+contentRouter.post("/summary",async(req,res)=>{
+    const {text}=req.body
+    
+    try {
+        const requestbody={
+            "model": "gpt-3.5-turbo",
+          
+            "messages": [
+                {
+                    "role": "user", 
+                    "content": `Summarize the provided ${text}`
+                }
+            ],
+            "temperature": 0.7
+    
+        }
+     const response= await axios.post("https://api.openai.com/v1/chat/completions",requestbody,
+           {headers:{
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.API_KEY}`
+            }}
+            )
+        const summarize=response.data.choices[0].message.content
+        res.json({summarize})
+        console.log(summarize)
+
+        
+    } catch (error) {
+        res.status(400).send({"error":error})
+    }
+})
+contentRouter.post("/translate",async(req,res)=>{
+    const {text,language}=req.body
+    
+    try {
+        const requestbody={
+            "model": "gpt-3.5-turbo",
+          
+            "messages": [
+                {
+                    "role": "user", 
+                    "content": `Translate the ${text} to ${language} language `
+                }
+            ],
+            "temperature": 0.7
+    
+        }
+     const response= await axios.post("https://api.openai.com/v1/chat/completions",requestbody,
+           {headers:{
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.API_KEY}`
+            }}
+            )
+        const translate=response.data.choices[0].message.content
+        res.json({translate})
+        console.log(translate)
 
         
     } catch (error) {
