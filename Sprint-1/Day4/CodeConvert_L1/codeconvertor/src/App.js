563ed93d7f01aa433e83819b4b3f3088a63e9507
kkalyankumar9 <kummarikalyan010@gmail.com>
2023-11-01 10:30:34 +0530

    codeconvertor

--- Sprint-1/Day4/CodeConvert_L1/codeconvertor/src/App.js
+++ Sprint-1/Day4/CodeConvert_L1/codeconvertor/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import CodeConveter from './Component/codeConveter';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
+       <Box bgColor="#F5F5F5" minH="100vh">
       <CodeConveter/>
+      </Box>
          </div>
   );
 }
--- Sprint-1/Day4/CodeConvert_L1/codeconvertor/src/Component/codeConveter.jsx
+++ Sprint-1/Day4/CodeConvert_L1/codeconvertor/src/Component/codeConveter.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Box, Button, Select } from '@chakra-ui/react';
+import { Box, Button,Text, Flex, Heading, Select } from '@chakra-ui/react';
 import MonacoEditor from 'react-monaco-editor';
 import { Spinner } from '@chakra-ui/react'
 const CodeConveter = () => {
@@ -38,8 +38,9 @@ const CodeConveter = () => {
               console.log(response.data.debuggedCode)
             })
             .catch((error) => {
-              console.log(error);
-            });
+              console.error("An error occurred:", error);
+              // Display an error message to the user
+          });
 
     }
     const handleQuality=()=>{
@@ -52,14 +53,19 @@ const CodeConveter = () => {
               console.log(response.data.qualityEvaluation)
             })
             .catch((error) => {
-              console.log(error);
-            });
+              console.error("An error occurred:", error);
+
+          });
 
     }
   return (
-    <Box m={4}>
-    <form onSubmit={handleConvert}>
-      <Select placeholder="Select Language" value={lan} onChange={(e) => setLang(e.target.value)}>
+    <Box >
+    <Heading color={"teal"}>Code Convertor</Heading>
+    <Flex  direction={['column', 'row']} wrap={"wrap"} textAlign={"center"} justify={"center"}     >
+
+    <Box m={4} textAlign={"center"} >
+    <form onSubmit={handleConvert} p={5}  >
+      <Select placeholder="Select Language" value={lan} onChange={(e) => setLang(e.target.value)} bgColor={"white"} w={"200px"} textAlign={"center"} p={2}>
         <option value="javascript">JavaScript</option>
         <option value="java">Java</option>
         <option value="python">Python</option>
@@ -67,7 +73,7 @@ const CodeConveter = () => {
       </Select>
 
       <MonacoEditor
-        width="800"
+        width="700"
         height="400"
       
         language="javascript"
@@ -82,20 +88,25 @@ const CodeConveter = () => {
         }}
         onChange={(value) => setCode(value)}
       />
+      <Flex p={5} ml={4}  direction={['column', 'row']} justifyContent={"space-around"} >
 
-      <Button type='submit'>Convert</Button>
-      <Button onClick={handleDebug}>Debug</Button>
-    <Button onClick={handleQuality}>Check quality</Button>
-
-
-    </form>
+      <Button  ml={4} type='submit'>Convert</Button>
    
+    
+    <Button ml={4} onClick={handleDebug}>Debug</Button>
+    <Button  ml={4} onClick={handleQuality}>Check quality</Button>
+    </Flex>
+    </form>
+    </Box>
 
-    <Box m={4}>
+    <Box mt={8}  p={4}  bgColor={"white"}w={"500px"} boxShadow=" rgba(0, 0, 0, 0.16) 0px 1px 4px">
                 {result.split('\n').map((line, index) => (
-                    <Box key={index}>{line}</Box>
+                    <Box key={index} textAlign={"left"} w={"100%"} p={1}>{line}</Box>
                 ))}
+              
             </Box>
+
+  </Flex>
   </Box>
   )
 }
